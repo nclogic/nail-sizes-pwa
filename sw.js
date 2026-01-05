766c71cb2865e469828c3e130f6b74d52eb4a87c
Michael <102840165+nclogic@users.noreply.github.com>
2026-01-05 01:02:10 -0500

    Update sw.js
    Edited app.js and edited images.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = "nail-sizes-v1";
+const CACHE_NAME = "nail-sizes-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -38,3 +38,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(req).then(cached => cached || fetch(req))
   );
 });
+
